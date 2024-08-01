d0d3d9a89263cc1b977fa3feba8d0ce7e5d39df4
faisalnawaz89 <isfaisalnawaz@gmail.com>
2024-08-02 00:43:28 +0530

    add css code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Pages/Home.jsx'
